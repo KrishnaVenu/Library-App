77924434675b0b5b8299ba5d175259e534716df3
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-06-21 21:05:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,51 @@
 const express =require('express');
+const homeRouter = express.Router();
 const app = express();
+const nav = [
+    {
+        link:'/login',name:'Login'
+    },
+    {
+        link:'/registration',name:'Sign Up'
+    }
+];
+const authorsRouter = require("./src/routes/authorRoutes")(nav);
+const booksRouter = require("./src/routes/bookRoutes")(nav);
 app.use(express.static('./public'));
 app.set("view engine","ejs");
 app.set("views","./src/views");
-//app.use('/books',booksRouter);
+app.use("/home",homeRouter);
+app.use("/authors",authorsRouter);
+app.use('/books',booksRouter);
 app.get('/',function(req,res){
     res.render("index",
     {
-        nav:[{link:'/login',name:'Login'},{link:'/registration',name:'Sign Up'}],
-        title:'Library Management'
+        nav,
+        title:'V.K.Library'
+    });
+});
+app.get('/login',function(req,res){
+    res.render("login",
+    {
+        nav,
+        title:'V.K.Library -Login'
     });
 });
 app.get('/registration',function(req,res){
-    res.render("registration",{
-        nav:[{link:'/login',name:'Login'},{link:'/registration',name:'Sign Up'}],
-        title:'Library Management-Registration'
+    res.render("registration",
+    {
+        nav,
+        title:'V.K.Library-Registration'
     });
 });
-app.get('/registration',function(req,res){
-    res.render("login",{
-        nav:[{link:'/login',name:'Login'},{link:'/registration',name:'Sign Up'}],
-        title:'Library Management-Login'
+//function router(nav){
+homeRouter.get('/',function(req,res){
+    res.render("home",
+    {
+        
+     nav:[{link:'/authors',name:'Authors'},{link:'/books',name:'Books'}],
+        title:'V.K.Library'
     });
 });
-
+//}
 app.listen(3222);
