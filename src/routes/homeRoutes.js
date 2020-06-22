ff7d3de1f944b0b891ff65f5263ee81461c95db9
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-06-22 21:05:26 +0530

    Update homeRoutes.js

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,14 +1,21 @@
 const express =require('express');
+const nav2 =[
+    {
+        link:'/authors',name:'Authors'
+    },
+    {
+        link:'/books',name:'Books'
+    }
+];
 const homeRouter = express.Router();
-function router(nav){
+function router(nav2){
     homeRouter.get('/',function(req,res){
         res.render("home",
         {
-            
-         nav:[{link:'/authors',name:'Authors'},{link:'/books',name:'Books'}],
+            nav2,
             title:'V.K.Library'
         });
     });
     return homeRouter;
-}
+} 
 module.exports = router;   
