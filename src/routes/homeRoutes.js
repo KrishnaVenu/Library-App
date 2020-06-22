6d3328b85f127e8a6fcd53e48c8def1b1d123833
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-06-22 16:20:58 +0530

    Add files via upload

---
+++ src/routes/homeRoutes.js
@@ -0,0 +1,12 @@
+const express =require('express');
+const homeRouter = express.Router();
+function router(nav){
+    homeRouter.get('/',function(req,res){
+        res.render("home",
+        {
+            
+         nav:[{link:'/authors',name:'Authors'},{link:'/books',name:'Books'}],
+            title:'V.K.Library'
+        });
+    });
+}    
